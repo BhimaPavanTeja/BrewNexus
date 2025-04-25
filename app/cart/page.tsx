4d73e89b8011fe8160f58fd99106d9f9e57e38c8
pavan <124360829+BhimaPavanTeja@users.noreply.github.com>
2025-04-25 14:04:22 +0530

    Update page.tsx

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -22,13 +22,13 @@ export default function Page() {
                 key={index}
                 className="itemCard h-[300px] w-[280px] m-5 shadow-lg"
               >
-                <div className="top w-full border-3 h-[50px] border-[#4B382A] bg-[#C49A6C] text-center text-xl pt-3 shadow-lg">
+                <div className="top w-full border-3 h-[50px] border-[#4B382A] bg-[#C49A6C] text-center text-xl pt-3 shadow-lg rounded-t-lg">
                   {item.name} | ₹{item.price}
                 </div>
                 <div className="middle w-full h-[200px]">
                   <Image
                     src={item.imgSrc}
-                    className="object-cover w-full h-full"
+                    className="object-cover w-full h-full rounded-b-lg"
                     alt={item.name}
                     width={280}
                     height={200}
@@ -37,7 +37,7 @@ export default function Page() {
                 <div className="bottom w-full">
                   <button
                     onClick={() => removeFromCart(item.name)}
-                    className="w-full cursor-pointer bg-red-600 h-[50px] text-white text-2xl mt-3"
+                    className="w-full cursor-pointer bg-red-600 h-[50px] text-white text-2xl mt-3 rounded-lg"
                   >
                     REMOVE
                   </button>
